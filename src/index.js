e00e98efe0c60f342fd313fdbde3c4afdfaf6440
Marco Moschettini <m.marmos@gmail.com>
2016-04-05 17:29:26 +0200

    New ps object. To be tested.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
 var fs = require('fs');
-var ps = require('ps-nodejs');
+var exec = require('child_process').exec;
+
+var ps = {
+  lookup: function(pid, callback)
+  {
+    if(/^win/.test(process.platform))
+    {
+      exec('tasklist /FI "PID eq ' + pid + '" /NH' , function(error, commands)
+      {
+        callback(error,commands.split(' ')[0]);
+      });
+    }
+    else
+    {
+      exec('ps aux | grep ' + pid + ' | grep -v \'grep\' | awk \'{print $11}\'', function(error, command)
+      {
+        callback(error, command);
+      });
+    }
+  }
+};
 
 function __command__(pid)
 {
   'use strict';
 
-  return new Promise(function(resolve)
+  return new Promise(function(resolve, reject)
   {
-    ps.lookup({pid: pid}, function(error, procs)
+    ps.lookup(pid, function(error, command)
     {
-      var command = '';
-
-      procs.forEach(function(proc)
-      {
-        if(proc.pid === pid)
-          command = proc.command;
-      });
-
-      resolve(command);
+      if(error)
+        reject(error);
+      else
+        resolve(command);
     });
   });
 }
